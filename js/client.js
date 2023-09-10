5a0adff2931a97810196b65da770382e51bb694a
Nicholas Galauxy <nicholas@galauxy.com>
2023-09-10 12:57:05 -0500

    Update client.js

--- js/client.js
+++ js/client.js
@@ -2,13 +2,18 @@ var Promise = TrelloPowerUp.Promise;
 
 var BLACK_ROCKET_ICON = 'https://cdn.glitch.com/1b42d7fe-bda8-4af8-a6c8-eff0cea9e08a%2Frocket-ship.png?1494946700421';
 
+const memberSortable = card => {return card.members.length + card.members.map(m => m.fullName).sort()};
 const memberSort = {
   text: "Members",
   callback: (t, list) => {
     return {
       sortedIds: list
         .cards
-        .sort(card => card.members.length + card.members.map(m => m.fullName))
+        .sort((a,b) => {
+            const a1 = memberSortable(a);
+            const b1 = memberSortable(b);
+            return a1 < b1 ? -1 : (b1 > a1 ? 1 : 0);
+        })
         .map(c => c.id)
     };
   },
