eb631872a942e1571e6fb5cee6bd688861b46f5d
Nicholas Galauxy <nicholas.galauxy@affirm.com>
2026-02-09 00:53:19 -0600

    Set Trello API key

--- js/config.js
+++ js/config.js
@@ -1,5 +1,5 @@
 /* Shared configuration for the Trello Power-Up */
 
 // Replace with your Trello API key from https://trello.com/power-ups/admin
-var APP_KEY = 'YOUR_APP_KEY_HERE';
+var APP_KEY = '601d8a467912ec64947041cc6ed2e3ef';
 var APP_NAME = 'Sort By Members';
